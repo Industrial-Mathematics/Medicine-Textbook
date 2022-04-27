efd308ae6729ba2d087fc50ff19103277983f594
Industrial-Mathematics <103092411+Industrial-Mathematics@users.noreply.github.com>
2022-04-27 10:18:53 +0900

    Add files via upload

---
+++ diagram.py
@@ -0,0 +1,12 @@
+import numpy as np
+from ripser import ripser
+from persim import plot_diagrams
+import matplotlib.pyplot as plt
+
+
+def diagram():
+    X = np.array(eval(input('데이터 집합 {(a,b), (c,d), (e,f)}를 [[a,b], [c,d], [e,f]]의 형태로 입력해주세요.')))
+    n = int(input('지속구간 다이어그램의 최대 차원을 적어주세요. 단, 숫자만 적어주세요. 예) 2'))
+    return plot_diagrams(ripser(X, maxdim=n)['dgms'])
+
+
